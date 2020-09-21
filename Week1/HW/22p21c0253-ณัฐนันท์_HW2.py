f77d495bbdd3aa4060270ac0ce949f8f13347b18
RTae <potae02@gmail.com>
2020-09-21 13:25:24 +0700

    Update 22p21c0253-ณัฐนันท์_HW2.py

--- Week1/HW/22p21c0253-ณัฐนันท์_HW2.py
+++ Week1/HW/22p21c0253-ณัฐนันท์_HW2.py
@@ -3,74 +3,56 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.animation
+import numpy as np 
 
-def DFSUtil(G, v, visited, sl): 
-	visited[v] = True
-	sl.append(v) 
-	for i in G[v]:
-		if visited[i] == False:
-			DFSUtil(G, i, visited, sl)
-	return sl
- 
-def DFS(G, source): 
-    visited = [False]*(len(G.nodes()))
-    sl = []
-    dfs_stack = []
-    dfs_stack.append(DFSUtil(G, source, visited, sl))
-    for i in range(len(G.nodes())):
-        if visited[i] == False:
-            sl = []
-            dfs_stk.append(DFSUtil(G, i, visited, sl))
-    return dfs_stack
-			
-def createGraph():
+def DFSUtil(nextNodes,path,pathNodeList,node,parent):
+  path = path + [node]
+  pathNodeList.append(path)
+  next_nodes = nextNodes.get(node)
+  if next_nodes != None:
+    for nextNode in nextNodes[node]:
+      if nextNode != parent:
+        DFSUtil(nextNodes,path,pathNodeList,nextNode,node)
+
+def DFS(Graph,start):
+    nextNodes = {node:list(dict(Graph[node]).keys()) for node in Graph}
+    pathNodeList = []
+    pathList = []
+    DFSUtil(nextNodes,[],pathNodeList,start,-1)
+    for paths in pathNodeList:
+        if len(paths) > 1:
+            for node_idx in range(len(paths)-1):
+                if (paths[node_idx],paths[node_idx+1]) not in pathList:
+                    pathList.append((paths[node_idx],paths[node_idx+1]))
+    return pathList
+    			
+def createGraph(edges):
     G = nx.Graph()
-    source = 1
-    G.add_edge(0, 1)
-    G.add_edge(0, 2)
-    G.add_edge(1, 3)
-    G.add_edge(3, 4)
-    G.add_edge(3, 5)
-    G.add_edge(2, 5)
-    return G, source
+    for e in edges:
+        G.add_edge(e[0],e[1])
+    return G
+
+edges = [[0,1],[0,2],[0,3],[3,5],[2,6],[6,7],[3,8],[1,9],[2,10]]
 
 fig, ax = plt.subplots(figsize=(6,6))
-G, source = createGraph()
-dfs_stk = DFS(G, source)
-
-def createEdgeList(dfs_stk,G):
-    edgelist = []
-    for i in dfs_stk:
-        for j in i[ :(len(i)-1)]:
-            edge = ()
-            if i[i.index(j)+1] in G[j]:
-                edge = (j,i[i.index(j)+1])
-                edgelist.append(edge)
-            else:
-                for k in i[1::-1]:
-                    if k in G[j]:
-                        edge = (j,k)
-                        edgelist.append(edge)
-                        break
-            
-    return edgelist
-
-edgelist = createEdgeList(dfs_stk,G)
+G = createGraph(edges)
+startNode = 0
+dfsPathList = DFS(G,startNode)
 pos = nx.spring_layout(G)
 def update(num):
+    np.random.seed(2)
     nx.draw(G, pos, with_labels = True)
-    counter = num % len(edgelist) + 1
-    paths = edgelist[:counter]
+    paths = dfsPathList[:num]
     stack = []
     for path in paths:
         for node in path:
             if node not in stack:
                 stack.append(node)
     nx.draw_networkx_edges(G, pos, edgelist = paths, edge_color = 'r')
-    ax.set_title("Start DFS at node 1"+"\n"+str(stack) , fontweight="bold")
+    ax.set_title("Start DFS at node "+str(startNode)+"\n"+str(stack) , fontweight="bold")
     ax.set_xticks([])
     ax.set_yticks([])
 
-ani = matplotlib.animation.FuncAnimation(fig, update, frames=6, interval=1000, repeat=True)
+ani = matplotlib.animation.FuncAnimation(fig, update, frames=len(dfsPathList)+1, interval=1000, repeat=True)
 ani.save('22p21c0253-ณัฐนันท์_HW2.gif', writer='imagemagick', fps=1)
 plt.show()
