c3fea7bdf625ce6d6450e841b0f6c02d65733d8c
RTae <potae02@gmail.com>
2020-09-26 03:09:52 +0700

    Update 22p21c0253-ณัฐนันท์_HW1.py

--- Week1/HW/22p21c0253-ณัฐนันท์_HW1.py
+++ Week1/HW/22p21c0253-ณัฐนันท์_HW1.py
@@ -1,148 +1,104 @@
 #22p21c0253-ณัฐนันท์ 
 
-import numpy as np
-import heapq
+from queue import PriorityQueue as pq
+from copy import deepcopy
 
-class Board:
-	parent = None
-	state = None
-	operator = None
-	depth = 0
-	zero = None
-	cost = 0
+class Node(object):
+    def __init__(self, board, move,parent=None):
+        self.board = board
+        self.move = move
+        self.parent = parent
 
-	def __init__(self, state, parent = None, operator = None, depth = 0):
-		self.parent = parent
-		self.state = np.array(state)
-		self.operator =  operator
-		self.depth = depth
-		self.zero = self.find_0()
-		self.cost = self.depth + self.manhattan()
-	
-	def __lt__(self, other):
-		if self.cost != other.cost:
-			return self.cost < other.cost
-		else:
-			op_pr = {'Up': 0, 'Down': 1, 'Left': 2, 'Right': 3}
-			return op_pr[self.operator] < op_pr[other.operator]
+    def __lt__(self,other): 
+        return 0
 
-	def __str__(self):
-		return str(self.state[:3]) + '\n' \
-				+ str(self.state[3:6]) + '\n' \
-				+ str(self.state[6:])+'\n' 
+    def find_zero(self):
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == 0:
+                    return i,j
 
-	def goal_test(self):
-		if np.array_equal(self.state, np.arange(9)):
-			return True
-		else:
-			return False
+    def manhattan_distance(self):
+        cost = 0
+        for i in range(3):
+            for j in range(3):
+                row = int(self.board[i][j]/3)
+                column = int(self.board[i][j]%3)
+                cost += abs(i-row)+abs(j-column)
+        return cost
 
-	def find_0(self):
-		for i in range(9):
-			if self.state[i] == 0:
-				return i
+    def generate_nodes(self):
+        find_zero = self.find_zero()
+        board = self.board
+        allnodes = []
+        if not find_zero[0]-1<0:
+            board_up = deepcopy(board)
+            board_up[find_zero[0]][find_zero[1]] = board_up[find_zero[0]-1][find_zero[1]]
+            board_up[find_zero[0]-1][find_zero[1]] = 0
+            board_up_node = Node(board_up,'down',self) 
+            allnodes.append(board_up_node)
+        if find_zero[0]+1<3:
+            board_down = deepcopy(board)
+            board_down[find_zero[0]][find_zero[1]] = board_down[find_zero[0]+1][find_zero[1]]
+            board_down[find_zero[0]+1][find_zero[1]] = 0
+            board_down_node = Node(board_down, 'up',self)
+            allnodes.append(board_down_node)
+        if not find_zero[1]-1<0:
+            board_left = deepcopy(board)
+            board_left[find_zero[0]][find_zero[1]] = board_left[find_zero[0]][find_zero[1]-1]
+            board_left[find_zero[0]][find_zero[1]-1] = 0
+            board_left_node = Node(board_left,'right',self)
+            allnodes.append(board_left_node)
+        if find_zero[1]+1<3:
+            board_right = deepcopy(board)
+            board_right[find_zero[0]][find_zero[1]] = board_right[find_zero[0]][find_zero[1]+1]
+            board_right[find_zero[0]][find_zero[1]+1] = 0
+            board_right_node = Node(board_right,'left',self)
+            allnodes.append(board_right_node)
+        
+        return allnodes
 
-	def manhattan(self):
-		state = self.index(self.state)
-		goal = self.index(np.arange(9))
-		return sum((abs(state // 3 - goal // 3) + abs(state % 3 - goal % 3))[1:])
+    def track(self):
+        path = []
+        path.append((self.move,self.board))
+        n = self.parent
+        while n.parent is not None:
+            path.append((n.move,n.board))
+            n = n.parent
+        path.append((n.move,n.board))
+        path.reverse()
+        return path
 
-	def index(self, state):
-	    index = np.array(range(9))
-	    for x, y in enumerate(state):
-	        index[y] = x
-	    return index
+class Astar():
+    def solve(self,initial_node):
+        PQ = pq()
+        visited = []
+        explored = 0
+        PQ.put((initial_node.manhattan_distance(),initial_node))
+        while not PQ.empty():  
+            h, n = PQ.get()
+            if n.board in visited:
+                continue
+            if h==0:
+                self.draw(n.track())
+                return
+        
+            visited.append(n.board)
+            explored+=1
+            for nnode in n.generate_nodes(): 
+                PQ.put((nnode.manhattan_distance(),nnode))
 
-	def swap(self, i, j):
-		new_state = np.array(self.state)
-		new_state[i], new_state[j] = new_state[j], new_state[i]
-		return new_state
+    def draw_board(self,board):
+        print(' ___________')
+        for plate in board:
+            print('| {} | {} | {} | '.format(plate[0],plate[1],plate[2]))
+        print(' ___________')
 
-	def up(self):
-		if self.zero > 2:
-			return Board(self.swap(self.zero, self.zero - 3), self, 'Up', self.depth + 1)
-		else:
-			return None	
-
-	def down(self):
-		if self.zero < 6:
-			return Board(self.swap(self.zero, self.zero + 3), self, 'Down', self.depth + 1)
-		else:
-			return None
-
-	def left(self):
-		if self.zero % 3 != 0:
-			return Board(self.swap(self.zero, self.zero - 1), self, 'Left', self.depth + 1)
-		else:
-			return None
-
-	def right(self):
-		if (self.zero + 1) % 3 != 0:
-			return Board(self.swap(self.zero, self.zero + 1), self, 'Right', self.depth + 1)
-		else:
-			return None
-
-	def neighbors(self):
-		neighbors = []
-		neighbors.append(self.up())
-		neighbors.append(self.down())
-		neighbors.append(self.left())
-		neighbors.append(self.right())
-		return list(filter(None, neighbors))
-
-	__repr__ = __str__
+    def draw(self,path):
+        for board in path:
+            self.draw_board(board[1])
 
 
-class Solver:
-	soln = None
-	path = None
-	nodes_expanded = 0
-	max_depth = 0
-
-	def ancestral_chain(self):
-		current = self.soln
-		chain = [current]
-		while current.parent != None:
-			chain.append(current.parent)
-			current = current.parent
-		return chain
-	
-	def path(self):
-		path = [node.operator for node in self.ancestral_chain()[-2::-1]]
-		return path
-
-	def ast(self, state):
-		frontier = []
-		heapq.heappush(frontier, state)
-		froxplored = set()
-		while frontier:
-			board = heapq.heappop(frontier)
-			froxplored.add(tuple(board.state))
-			print(board)
-			if board.goal_test():
-				self.soln = board
-				self.path = self.path()
-				self.nodes_expanded = len(froxplored)-len(frontier)-1
-				return self.soln
-			for neighbor in board.neighbors():
-				if tuple(neighbor.state) not in froxplored:
-					heapq.heappush(frontier, neighbor)
-					froxplored.add(tuple(neighbor.state))
-					self.max_depth = max(self.max_depth, neighbor.depth)
-		return None
-
-value = [1,2,5,3,4,0,6,7,8]
-
-p = Board(np.array(value))
-print("="*15)
-print("Input board")          
-print("="*15)
-print(p)
-
-print("="*15)
-s = Solver()
-soln = s.ast(p)
-print("="*15)
-print("Result board")          
-print("="*15)
-print(soln)
+initial_node = Node([[1,2,5],[3,4,0],[6,7,8]],'start')
+s = Astar()
+s.solve(initial_node)
